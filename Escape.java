b4fa5b510f03123bb25ae208c018621e61f7eddd
Maryam-Saba <105993857+Maryam-Saba@users.noreply.github.com>
2022-05-22 03:21:39 +0500

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,13 @@
+public class Escape
+{
+public static void main(String args[])
+{ 
+System.out.println("one\ntwo");
+System.out.println("one\roo1");
+System.out.println("one\b1");
+System.out.println("One\tTwo\tThree\tFour");
+System.out.println("\"Maryam Saba\"");
+System.out.println("\'M\'");
+System.out.println("\\BSCS\\");
+}
+}
